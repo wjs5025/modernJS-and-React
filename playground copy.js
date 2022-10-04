07980ba6e2bc5cd014222075f9c3212911238045
저니녁 <wjs_5025@naver.com>
2022-10-04 17:53:53 +0900

    add : 19. 클로저 내용 추가 100%

--- playground copy.js
+++ playground copy.js
@@ -1,7 +1,11 @@
-const arr = new Array(5).fill(3);
+var arr = [];
 
-arr.forEach((v, i, array) => {
-  console.log(v); 
-  console.log(i);
-  console.log(array);
-});
+for (var i = 0; i < 5; i++) {
+  arr[i] = function (el) {
+    return el;
+  };
+}
+
+for (var j = 0; j < arr.length; j++) {
+  console.log(arr[j](j));
+}
