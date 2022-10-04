fa1846062635a85736d7319f141591db7e3e19b5
wjs5025 <wjs_5025@naver.com>
2022-10-04 23:08:00 +0900

    update : readme / 10.04 발표 진행

--- playground.js
+++ playground.js
@@ -1,32 +1,13 @@
-// 함수를 인자로 전달받고 함수를 반환하는 고차 함수
-// 이 함수가 반환하는 함수는 클로저로서 카운트 상태를 유지하기 위한 자유 변수 counter을 기억한다.
-function makeCounter(predicate) {
-  // 카운트 상태를 유지하기 위한 자유 변수
-  var counter = 0;
-  // 클로저를 반환
-  return function () {
-    counter = predicate(counter);
-    return counter;
+const arr = [];
+
+for (let i = 0; i < 5; i++) {
+  arr[i] = function () {
+    return i;
   };
 }
 
-// 보조 함수
-function increase(n) {
-  return ++n;
+for (let j = 0; j < arr.length; j++) {
+  console.log(arr[j]());
 }
 
-// 보조 함수
-function decrease(n) {
-  return --n;
-}
-
-// 함수로 함수를 생성한다.
-// makeCounter 함수는 보조 함수를 인자로 전달받아 함수를 반환한다
-let increaser = makeCounter(increase);
-console.log(increaser()); // 1
-console.log(increaser()); // 2
-
-// increaser 함수와는 별개의 독립된 렉시컬 환경을 갖기 때문에 카운터 상태가 연동하지 않는다.
-increaser = makeCounter(decrease);
-console.log(increaser()); // -1
-console.log(increaser()); // -2
+// 클로저는 스코프체인
