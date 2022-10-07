502bc701fd8efaa3f47836157608b5fa304670c4
wjs5025 <wjs_5025@naver.com>
2022-10-07 22:29:26 +0900

    add : 21. 빌트인 객체, 22. 전역객체 발표 진행

--- playground copy.js
+++ playground copy.js
@@ -1,11 +1,1 @@
-var arr = [];
-
-for (var i = 0; i < 5; i++) {
-  arr[i] = function (el) {
-    return el;
-  };
-}
-
-for (var j = 0; j < arr.length; j++) {
-  console.log(arr[j](j));
-}
+console.log(parseInt("F", 16));
